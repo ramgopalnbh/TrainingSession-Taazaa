a7299ce8e4fda711a786796c0c1ed428998fa0a9
RamGopal <56515607+ramgopalnbh@users.noreply.github.com>
2021-03-24 00:27:01 +0530

    Day-8 Assignment Extension and Delegates

---
+++ RamGopal/Core/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+    public class Product
+    {
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ RamGopal/CoreConsoleApp/MyExtensionDemo.cs
@@ -0,0 +1,17 @@
+﻿using Core;
+using Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreConsoleApp
+{
+    static class MyExtensionDemo
+    {
+        public static void displayProduct(this Inventory i, Product p)
+        {
+            i.AddProduct(p.Title, p.Price, p.Color);
+            Console.WriteLine(i.ToString());
+        }
+    }
+}
