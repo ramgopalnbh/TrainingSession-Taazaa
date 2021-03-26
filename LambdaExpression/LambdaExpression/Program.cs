fc04a956854343d5b5d2deaa184c73f110885fb9
RamGopal <56515607+ramgopalnbh@users.noreply.github.com>
2021-03-26 14:19:46 +0530

    Simple Lambda Expression Tree

---
+++ LambdaExpression/LambdaExpression/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LambdaExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Func<int, int> square =(x) =>{return x*x; };
+
+            Func<int, int> square = (x) => x * x;
+            System.Linq.Expressions.Expression<Func<int, int>> e = x => x * x;
+            var a = e.Compile();
+       
+            Console.WriteLine(e);
+            Console.WriteLine(a(8));
+        }
+    }
+}
